75c6464040e7ee270a6c23498d2cdc1c0b4150e2
Luca Poulos <lucapoulos@berkeley.edu>
2023-12-06 00:34:00 -0800

    day 5 progress

--- day5/day5.py
+++ day5/day5.py
@@ -1,10 +1,29 @@
 from sys import stdin
 
+
+def process(seeds, rmap):
+    rseed = list()
+    for m in rmap:
+        for seed in seeds:
+            if seed in m:
+                rseed.append(m[seed][m.index(seed)])
+    return rseed
+
+
 data = stdin.readlines()
 
 seeds = data[0].split(": ")[1].split()
-print(seeds)
 
-# def process(seeds, *map):
+i = 3
+range_queue = list()
+rmap = list()
+while i < len(data):
+    if data[i][0].isdigit():
+        dest_start, source_start, rlen = (int(x) for x in data[i].strip('\n').split())
+        rmap.append({range(source_start, source_start + rlen): range(dest_start, dest_start + rlen)})
 
+    if data[i][0].isalpha():
+        seeds = process(seeds, rmap)
+        print(seeds)
+    i += 1
 
